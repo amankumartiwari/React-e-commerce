cf18d3ab53cee1d87f1b384ee255ccb583ebf4e5
Aman Kumar Tiwari <kumaramantiwari98@gmail.com>
2020-05-06 22:40:27 +0530

    pages added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
+///import logo from './logo.svg';
 import './App.css';
+import MultiStepForm from './Components/MultistepForm/MultistepForm';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="wrapper">
+        <MultiStepForm />
+      </div>
     </div>
   );
 }
-
 export default App;
